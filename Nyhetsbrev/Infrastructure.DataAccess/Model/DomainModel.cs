8ab69060898b92f8b14a001ab13354325ebe2a73
Tom Erik Sortland <57662099+tomeriksortland@users.noreply.github.com>
2020-08-26 15:20:24 +0200

    hf

---
+++ Nyhetsbrev/Infrastructure.DataAccess/Model/DomainModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.DataAccess.Model
+{
+    public class DomainModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public string VerificationCode { get; set; }
+
+        public DomainModel(Guid id, string name, string email, bool active, string verificationCode)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Active = active;
+            VerificationCode = verificationCode;
+        }
+
+        public DomainModel()
+        {
+            
+        }
+    }
+}
--- Nyhetsbrev/Nyhetsbrev/wwwroot/JS/Verify.js
+++ Nyhetsbrev/Nyhetsbrev/wwwroot/JS/Verify.js
@@ -1,12 +1,19 @@
 ﻿
+if (location.search.includes("email") && location.search.includes("code")) {
+    console.log("hei");
+    ConfirmSubscription();
+}
+
+
 async function ConfirmSubscription() {
 
-    const querryString = window.location.search;
-    const urlParams = new URLSearchParams(querryString);
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
     const emailParam = urlParams.get("email");
-    const verificationCodeParam = urlParams.get("verificationCode");
+    const verificationCodeParam = urlParams.get("code");
+    console.log("HAlla pårrrrrreeeeeøeøeøeeøeø");
     console.log(verificationCodeParam);
     console.log(emailParam);
-    //var response = axios.patch("/api/Subscribe", { Email: emailParam, VerificationCode: verificationCodeParam });
+    //var response = await axios.patch("/api/Subscribe", { Email: emailParam, VerificationCode: verificationCodeParam });
     //console.log(response.data);
 }
