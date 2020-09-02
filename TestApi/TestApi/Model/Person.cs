b9de419adb56f4ae4dbaaa9bbc3a25052a7233bd
Tom Erik Sortland <57662099+tomeriksortland@users.noreply.github.com>
2020-09-02 08:50:31 +0200

    k

--- TestApi/TestApi/Model/Person.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestApi.Model
-{
-    public class Person
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
